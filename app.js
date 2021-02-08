f488ff0b8bd572f0b31f0f64ecd937c7cbf49276
rayhanulislampatwary <rayhanulislampatwary@gmail.com>
2021-02-08 20:59:20 +0600

    edited single meal part

--- app.js
+++ app.js
@@ -27,11 +27,13 @@ function searchMeal(e) {
              mealsEl.innerHTML = data.meals
             .map(
                 (meal) => `
-                <div class = "meal" >
-                <div class="meal-box">
+                <div class = "meal" onclick="singleItem('${meal.strMeal}')">
+                <div class="meal-box" >
                 <img class="meal-image" src="${meal.strMealThumb}" />
                 <h3 class="d-flex justify-content-center">${meal.strMeal}</h3>
+                <button class="btn btn-success d-flex justify-content-center" onclick="singleItem('${meal.strMeal}')"> Details </button>
                 </div>
+                
                 </div>
                 `
             )
@@ -48,5 +50,39 @@ function searchMeal(e) {
   }
 }
 
+//EventListener
   submit.addEventListener("submit", searchMeal);
 
+
+// single item show 
+const singleItem = name =>{
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => singleItemShow(data));
+  
+}
+
+const singleItemShow = item => {
+  const mealSingleItem=item.meals[0];
+  single_mealEl.innerHTML = `
+  <div class="d-flex justify-content-center ">
+  <div class="single-card">
+  <img class="single-meal-img" src="${mealSingleItem.strMealThumb}">
+  <h1 class="d-flex justify-content-center">${mealSingleItem.strMeal}</h1>
+  <h2> Ingredients </h2> 
+  <h6><i class="fas fa-chevron-circle-right"></i> ${mealSingleItem.strIngredient1}</h6>
+  <h6><i class="fas fa-chevron-circle-right"></i> ${mealSingleItem.strIngredient2}</h6>
+  <h6><i class="fas fa-chevron-circle-right"></i> ${mealSingleItem.strIngredient3}</h6>
+  <h6><i class="fas fa-chevron-circle-right"></i> ${mealSingleItem.strIngredient4}</h6>
+  <h6><i class="fas fa-chevron-circle-right"></i> ${mealSingleItem.strIngredient5}</h6>
+  <h6><i class="fas fa-chevron-circle-right"></i> ${mealSingleItem.strIngredient6}</h6>
+  <h6><i class="fas fa-chevron-circle-right"></i> ${mealSingleItem.strIngredient7}</h6>
+  <h6><i class="fas fa-chevron-circle-right"></i> ${mealSingleItem.strIngredient8}</h6>
+  <h6><i class="fas fa-chevron-circle-right"></i> ${mealSingleItem.strIngredient9}</h6>
+  <h6><i class="fas fa-chevron-circle-right"></i> ${mealSingleItem.strIngredient10}</h6>
+  </div>
+  </div>
+  `
+
+}
